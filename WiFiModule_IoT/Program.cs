ee3a2aa95125bf28f671a9a8380b85e43cf2de25
Андрій Єгожа <andrii.yehozha@nure.ua>
2018-12-22 04:56:33 +0200

    Create WiFi Module for IoT

--- WiFiModule_IoT/Program.cs
+++ WiFiModule_IoT/Program.cs
@@ -1,15 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO.Ports;
+using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using Newtonsoft.Json;
 
 namespace WiFiModule_IoT
 {
     class Program
     {
+        SerialPort sp = new SerialPort("COM4", 9600, Parity.None, 8, StopBits.One);
+
         static void Main(string[] args)
         {
+            new Program();
+        }
+
+        private Program()
+        {
+            try
+            {
+                sp.DataReceived += new SerialDataReceivedEventHandler(OnDataReceived);
+                sp.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Read();
+        }
+
+        private void OnDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Thread.Sleep(1000);
+            string result = sp.ReadExisting();
+            Indicator json = JsonConvert.DeserializeObject<Indicator>(result);
+            postIndicators(json);
+        }
+
+        private async void postIndicators(Indicator indicator)
+        {
+            string json = "{" + $"\"IdFridge\":{indicator.IdFridge}," +
+                                $"\"Temperature\":{indicator.Temperature.ToString().Replace(',', '.')}," +
+                                $"\"Humidity\":{indicator.Humidity.ToString().Replace(',', '.')}," +
+                          "}";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    await client.PostAsync(
+                        "https://medicalfridgeserver.azurewebsites.net/api/Indicators",
+                        new StringContent(json, Encoding.UTF8, "application/json")
+                        );
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
