38f818f55cff5e451e087267bc748aaff6fa75f2
Андрій Єгожа <andrii.yehozha@nure.ua>
2018-12-22 04:49:09 +0200

    Добавьте файлы проекта.

---
+++ WiFiModule_IoT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiFiModule_IoT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
